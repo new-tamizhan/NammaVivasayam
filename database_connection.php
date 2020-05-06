8f9022cdcd63d1854976a85b4eb15b29477afffd
sivaxp1 <47629179+sivaxp1@users.noreply.github.com>
2020-05-06 10:34:01 +0530

    Update database_connection.php

--- database_connection.php
+++ database_connection.php
@@ -2,6 +2,6 @@
 
 //database_connection.php
 
-$connect = new PDO("mysql:host=localhost;dbname=test", "root", "");
+$connect = new PDO("mysql:host=localhost;dbname=id9868355_newtamizhan", "id9868355_sivaa", "sivaa@1705");
 
 ?>
